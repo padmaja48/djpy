512bb31c40d8c5d68fde8bf4639f7cf229963a4b
padmaja48 <padmajakuncham48@gmail.com>
2025-09-30 20:11:54 +0530

    first commit

--- website/models.py
+++ website/models.py
@@ -9,7 +9,6 @@ class Products(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock=models.IntegerField()
-    image = models.ImageField(upload_to='products/', null=True, blank=True)
     def __str__(self):
         return self.name
 
